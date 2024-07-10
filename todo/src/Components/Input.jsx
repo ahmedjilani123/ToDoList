c8e1e2769136c6b97361ead18f660db9edb86357
ahmedjilani123 <ahmedjilani0011@gmail.com>
2024-07-10 12:34:34 +0530

    add placeholder

--- todo/src/Components/Input.jsx
+++ todo/src/Components/Input.jsx
@@ -33,7 +33,7 @@ const Input = () => {
             <div className='flex justify-center items-center mt-5'>
                 <div className='md:w-[800px] h-[6rem] bg-blue-500 rounded-lg p-6 flex shadow-lg '>
                     <div className='w-full flex bg-slate-300 justify-center rounded-l-md '>
-                        <input ref={InputElement} className='w-full text-black bg-slate-300 outline-none rounded-l-md px-4 text-lg ' />
+                        <input ref={InputElement} placeholder="add your task" className='w-full text-black bg-slate-300 outline-none rounded-l-md px-4 text-lg ' />
                     </div>
                     <div className='w-[5rem] flex bg-slate-300 justify-center rounded-r-lg  '>
                         <button className='text-sky-900 w-full bg-slate-300 rounded-r-lg font-bold active:text-blue-400 active:shadow-lg ' onKeyDown={AddTaskHandle} onClick={AddTaskHandle}>Add</button>
